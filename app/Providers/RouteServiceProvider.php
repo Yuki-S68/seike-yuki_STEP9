3911ed48d7044b7b9d61ae48838d13d31db045cd
Yuki <nukoton.68@gmail.com>
2026-09-14 23:30:04 +0900

    RouteServiceProvider.phpの作成

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * ログイン後の遷移先
+     */
+    public const HOME = '/products'; // ← 商品一覧へ遷移させる
+
+    /**
+     * ルートの読み込み
+     */
+    public function boot(): void
+    {
+        $this->routes(function () {
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+        });
+    }
+}
+
